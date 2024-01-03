f3e2b8684f8c4106acafba1947ba7cd1bb7825dd
PeakpoonPK <peakpoonamoroll@gmail.com>
2024-01-03 21:14:37 +0700

    finish to learning basic mongoose

---
+++ person.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/shopApp')
+    .then(() => {
+        console.log('CONNENTION OPEN!!!')
+    })
+    .catch(err => {
+        console.log("ON ON ERROR!!!")
+        console.log(err)
+    })
+
+const personSchema = new mongoose.Schema({
+    first: String,
+    last: String
+})
+
+personSchema.virtual('fullName').get(function () {
+
+    return `${this.first} ${this.last}`
+})
+
+personSchema.pre('save', async function () {
+    this.first = 'YO MAMA'
+    this.last = "HI HI"
+    console.log('JUST SAVED!!')
+})
+
+const Person = mongoose.model('Person', personSchema)
